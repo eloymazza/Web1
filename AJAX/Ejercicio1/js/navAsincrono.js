3b7d36bac825a5c195bbcba7df703d373571a4f1
Eloy Mazza <eloymazza.ra@gmail.com>
2017-05-30 20:04:37 -0300

    termine el primer ejercicio del practico de ajax

--- AJAX/Ejercicio1/js/navAsincrono.js
+++ AJAX/Ejercicio1/js/navAsincrono.js
@@ -1,12 +1,14 @@
 $("document").ready(function() {
-    
+    let nombreSeccion;
     $("a").on("click", function(){
+        nombreSeccion = event.target.id;
+        console.log(nombreSeccion);
         $.ajax({
-            "url": "http://localhost/Proyectos/ejercicio1/politica.html",
+            "url": "http://localhost/Proyectos/practicoAjaxPartialRender/" + nombreSeccion + ".html",
             "method": "GET",
             "dataType": "HTML",
             "success": function (data, textStatus) {
-                $(".contenedorNoticiaPrincipal").html(data);
+                $(".noticiaPrincipal").html(data);
             }
         });
         $(".contenedor").html("Cargando");
--- AJAX/Testing/js/partial.js
+++ AJAX/Testing/js/partial.js
@@ -2,17 +2,14 @@ $("document").ready(function(){
 
     $("#cargar").on("click", function () {
         $.ajax({
-            "url": "http://localhost/dashboard/404.html",
+            "url": "http://localhost/Proyectos/404.html",
             "method": "GET",
             "dataType": "HTML",
             "success": function (data, textStatus) {
-                console.log("entre");
-                $("#contenedor").html(data);
-                $(".js-comportamiento").on("click", function(){
-                });
+                
             }
         });
-        $("#contenedor").html("Cargando");
+        $(".js-contenedor").html("Cargando");
     });
 
 });
