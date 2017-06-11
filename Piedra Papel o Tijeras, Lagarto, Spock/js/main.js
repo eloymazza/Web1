4fb7477f0e673caea05f26326460f408bac6e1fb
Eloy Mazza <eloymazza.ra@gmail.com>
2017-06-11 19:05:00 -0300

    termine pantalla de seleccion

--- Piedra Papel o Tijeras, Lagarto, Spock/js/main.js
+++ Piedra Papel o Tijeras, Lagarto, Spock/js/main.js
@@ -1,5 +1,42 @@
 $("document").ready(function() {
 
-alert("hola mundo");
+    class Juego{
+
+        constructor(){
+            this.modo = "";
+            this.p1 = new Jugador();
+            this.p2 = new Jugador();
+            this.tabla = new TablaDeRelaciones();
+        }
+
+        setearModo(modoElegido){
+            this.modo = modoElegido;
+        }
+        jugar(){
+
+        }
+    }
+
+    class Jugador{
+
+        constructor(creditoInicial){
+            this.credito = creditoInicial;
+            this.eleccion = undefined;
+        }
+        actualizarCredito(cantidad){
+            this.credito += cantidad;
+        }
+        setEleccion(eleccion){
+            this.eleccion = eleccion;
+        }
+    }
+
+    class TablaDeRelaciones{
+
+        constructor(){
+            const empata = 1;
+        }
+
+    }
     
 });
